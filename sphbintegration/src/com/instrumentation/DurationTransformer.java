e75115c5c2cb91130e4b1ba70feeece35ac66ea7
Mukul-Moudgil <mukul.moudgil71@gmail.com>
2015-01-21 14:55:46 +0530

    Second Commit

--- sphbintegration/src/com/instrumentation/DurationTransformer.java
+++ sphbintegration/src/com/instrumentation/DurationTransformer.java
@@ -5,12 +5,9 @@ import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
 
-import javassist.CannotCompileException;
 import javassist.ClassPool;
-import javassist.CtBehavior;
 import javassist.CtClass;
 import javassist.CtMethod;
-import javassist.NotFoundException;
 
 //this class will be registered with instrumentation agent
 
@@ -22,46 +19,55 @@ public class DurationTransformer implements ClassFileTransformer {
             ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
         byte[] byteCode = classfileBuffer;
 
-        // since this transformer will be called when all the classes are
-        // loaded by the classloader, we are restricting the instrumentation
-        // using if block only for the Lion class
-        // System.out.println("Instrumenting...... class " + className);
-        if (className.equals("org/hibernate/SessionFactory")) {
+        if (className.equals("org/hibernate/impl/SessionFactoryImpl")) {
             System.out.println("Instrumenting...... class " + className);
             try {
                 ClassPool classPool = ClassPool.getDefault();
                 CtClass ctClass = classPool.makeClass(new ByteArrayInputStream(classfileBuffer));
-                // new 
-                CtBehavior[] methods2 = ctClass.getDeclaredBehaviors();
-                for (int i = 0; i < methods2.length; i++) {
-                	System.out.println(methods2[i].isEmpty());
-                  if (methods2[i].isEmpty() == false) {
-                    changeMethod(methods2[i]);
-                  }
-                }
-                
-                // new ends
+
                 CtMethod[] methods = ctClass.getDeclaredMethods();
 
                 for (CtMethod method : methods) {
-                    // System.out.println(method.getMethodInfo().getName());
+
                     if (method.getName().equals("openSession")) {
-                        // method.addLocalVariable("Logstatements", CtClass.charType);
-                        // method.insertBefore("Logstatements = in method call");
-                        // method.insertBefore("System.out.println(\"saveOrUpdate ");
-                        // method.insertBefore("System.out.println(\"get current session called ");
-                        //method.insertBefore("System.out.println(\"Here I am!\")");
-                        System.out.println("hello i am in open session");
+                        method.insertBefore("{ System.out.println(\"hello i am in open connection of SessionFactoryImpl\"); }");
 
                     }
                     if (method.getName().equals("close")) {
-                        System.out.println("i am in close");
+                        method.insertAfter("{ System.out.println(\"hello i am in Close session of SessionFactoryImpl\"); }");
                     }
-                    if (method.getName().equals("getStatistics")) {
-                        System.out.println("i am in getStatistics");
+
+                    if (method.getName().equals("getConnectionProvider")) {
+                        method.insertAfter("{ System.out.println(\"hello i am in getConnectionProvider of SessionFactoryImpl\"); }");
                     }
-                    if (method.getName().equals("getCurrentSession")) {
-                        System.out.println("i am in getCurrentSession");
+                }
+                byteCode = ctClass.toBytecode();
+                ctClass.detach();
+                System.out.println("Instrumentation complete : " + className);
+            } catch (Throwable ex) {
+                System.out.println("Exception: " + ex);
+                ex.printStackTrace();
+
+            }
+        }
+
+        if (className.equals("org/hibernate/impl/SessionImpl")) {
+            System.out.println("Instrumenting...... class " + className);
+            try {
+                ClassPool classPool = ClassPool.getDefault();
+                CtClass ctClass = classPool.makeClass(new ByteArrayInputStream(classfileBuffer));
+
+                CtMethod[] methods = ctClass.getDeclaredMethods();
+
+                for (CtMethod method : methods) {
+
+
+                    if (method.getName().equals("connection")) {
+                        method.insertBefore("{ System.out.println(\"hello i am in  connection method of SessionImpl\"); }");
+
+                    }
+                    if (method.getName().equals("close")) {
+                        method.insertAfter("{ System.out.println(\"hello i am in Close session of SessionImpl\"); }");
                     }
 
                 }
@@ -74,13 +80,38 @@ public class DurationTransformer implements ClassFileTransformer {
 
             }
         }
+
+        if (className.equals("org/hibernate/stat/StatisticsImpl")) {
+            System.out.println("Instrumenting...... class " + className);
+            try {
+                ClassPool classPool = ClassPool.getDefault();
+                CtClass ctClass = classPool.makeClass(new ByteArrayInputStream(classfileBuffer));
+
+                CtMethod[] methods = ctClass.getDeclaredMethods();
+
+                for (CtMethod method : methods) {
+
+                    if (method.getName().equals("getSessionOpenCount")) {
+                        System.out.println(method.getName().equals("getSessionOpenCount"));
+                        System.out.println("good bye !!");
+                        method.insertAfter("{ System.out.println(\"hello i am in getSessionOpenCountof StatisticsImpl+sessionOpenCount\"); }");
+
+                    }
+                    if (method.getName().equals("getSessionCloseCount")) {
+                        method.insertBefore("{ System.out.println(\"hello i am in getSessionOpenCount of StatisticsImpl+sessionCloseCount\"); }");
+                    }
+                }
+                byteCode = ctClass.toBytecode();
+                ctClass.detach();
+                System.out.println("Instrumentation complete : " + className);
+            } catch (Throwable ex) {
+                System.out.println("Exception: " + ex);
+                ex.printStackTrace();
+
+            }
+        }
+
+
         return byteCode;
     }
-    
-    private void changeMethod(CtBehavior method) throws NotFoundException, CannotCompileException {
-        if (method.getName().equals("openSession")) {
-          method.insertBefore("System.out.println(\"started method at \" + new java.util.Date());");
-          method.insertAfter("System.out.println(\"ended method at \" + new java.util.Date());");
-        }
-      }
 }
